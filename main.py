a8b565d6bc9b653bf553cad549d04bb5c82d5f58
mogbonna <mogbonna045@gmail.com>
2024-10-31 21:39:00 +0100

    Day 26: Add a Table

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-
     pdf = FPDF(orientation="p", unit="mm", format="A4")
     pdf.add_page()
 
@@ -18,6 +16,29 @@ for filepath in filepaths:
     pdf.cell(50,8,f"Invoice nr.{invoice_nr}", ln=1)
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(50,8,f"Date: {date}")
+    pdf.cell(50,8,f"Date: {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Add a header
+    columns = df.columns
+    columns = [item.replace('_', ' ').title() for item in columns]
+    pdf.set_font(family="Times", size=10, style='B')
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(30, 8, columns[0], border=1)
+    pdf.cell(70, 8, columns[1], border=1)
+    pdf.cell(30, 8, columns[2], border=1)
+    pdf.cell(30, 8, columns[3], border=1)
+    pdf.cell(30, 8, columns[4], border=1, ln=1)
+
+    # Add rows to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80,80,80)
+        pdf.cell(30, 8, txt=str(row["product_id"]), border=1)
+        pdf.cell(70, 8, txt=str(row["product_name"]), border=1)
+        pdf.cell(30, 8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(30, 8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(30, 8, txt=str(row["total_price"]), border=1, ln=1)
 
     pdf.output(f"PDFs/{filename}.pdf")
